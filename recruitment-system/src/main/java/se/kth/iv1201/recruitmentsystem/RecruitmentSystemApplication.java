24e1a2b6a305a14927b6fb8cf83e0c45f08ebdb8
Joey Öhman <joeyoh@kth.se>
2019-01-21 16:05:01 +0100

    project setup

---
+++ recruitment-system/src/main/java/se/kth/iv1201/recruitmentsystem/RecruitmentSystemApplication.java
@@ -0,0 +1,14 @@
+package se.kth.iv1201.recruitmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentSystemApplication.class, args);
+	}
+
+}
+
---
+++ recruitment-system/src/test/java/se/kth/iv1201/recruitmentsystem/RecruitmentSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package se.kth.iv1201.recruitmentsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecruitmentSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
