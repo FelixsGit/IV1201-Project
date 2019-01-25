6f60541aa38b11c77ee782f08f6caf1afc3785d8
Joey Öhman <joeyoh@kth.se>
2019-01-25 14:48:41 +0100

    Init testing

---
+++ recruitment-system/src/test/java/se/kth/iv1201/recruitmentsystem/presentation/ApplicationControllerTest.java
@@ -0,0 +1,49 @@
+package se.kth.iv1201.recruitmentsystem.presentation;
+
+import net.jcip.annotations.NotThreadSafe;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringJUnitWebConfig(initializers = ConfigFileApplicationContextInitializer.class)
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"se.kth.iv1201.recruitementsystem"})
+@NotThreadSafe
+
+@TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class,
+        ApplicationControllerTest.class,
+        TransactionalTestExecutionListener.class})
+
+public class ApplicationControllerTest implements TestExecutionListener {
+    @Autowired
+    private WebApplicationContext webappContext;
+
+    private MockMvc mockMvc;
+
+    @Override
+    public void beforeTestClass(TestContext testContext) {
+
+    }
+
+    @Override
+    public void afterTestClass(TestContext testContext) {
+
+    }
+
+    @BeforeEach
+    void setup() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webappContext).build();
+
+    }
+}
