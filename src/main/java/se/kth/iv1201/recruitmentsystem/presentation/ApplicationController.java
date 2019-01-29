846f54f53241fd5d9affa24c34ea6dc6dff0452b
Acander <adrianz@kth.se>
2019-01-29 11:55:11 +0100

    implement registration form and application controller

--- src/main/java/se/kth/iv1201/recruitmentsystem/presentation/ApplicationController.java
+++ src/main/java/se/kth/iv1201/recruitmentsystem/presentation/ApplicationController.java
@@ -1,13 +1,19 @@
 package se.kth.iv1201.recruitmentsystem.presentation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class ApplicationController {
 
     static final String DEFAULT_PAGE_URL = "/";
     static final String REGISTER_PAGE_URL = "register";
+    private static String REGISTER_FORM_OBJ_NAME = "registrationForm";
 
     /**
      * No page is specified, redirect to the welcome page.
@@ -20,8 +26,14 @@ public class ApplicationController {
     }
 
     @GetMapping(REGISTER_PAGE_URL)
-    public String showRegisterView() {
+    public String showRegisterView(Model model) {
+        if(!model.containsAttribute(REGISTER_FORM_OBJ_NAME)) {
+            model.addAttribute(new RegistrationForm());
+        }
         return REGISTER_PAGE_URL;
     }
+
+    @PostMapping(DEFAULT_PAGE_URL + REGISTER_PAGE_URL)
+    public String registerUser(@Valid RegistrationForm, BindingResult bindingResult, )
 
 }
--- src/main/java/se/kth/iv1201/recruitmentsystem/presentation/RegistrationForm.java
+++ src/main/java/se/kth/iv1201/recruitmentsystem/presentation/RegistrationForm.java
@@ -1,4 +1,76 @@
 package se.kth.iv1201.recruitmentsystem.presentation;
 
+import javax.validation.constraints.NotBlank;
+
 public class RegistrationForm {
+    private final String errMsg = "Needs to be filled in";
+
+    @NotBlank(message = errMsg)
+    private String name;
+
+    @NotBlank(message = errMsg)
+    private String surname;
+
+    @NotBlank(message = errMsg)
+    private String email;
+
+    @NotBlank(message = errMsg)
+    private String password;
+
+    @NotBlank(message = errMsg)
+    private String username;
+
+    @NotBlank(message = errMsg)
+    private String ssn;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+
+
 }
