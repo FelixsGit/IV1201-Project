f84adb2d3cc3d84b852e3d85a4d65564ec0ebf6e
FelixsGit <28272254+FelixsGit@users.noreply.github.com>
2019-01-31 13:30:54 +0100

    Add loginForm structure

--- src/main/java/se/kth/iv1201/recruitmentsystem/presentation/LoginForm.java
+++ src/main/java/se/kth/iv1201/recruitmentsystem/presentation/LoginForm.java
@@ -1,4 +1,29 @@
 package se.kth.iv1201.recruitmentsystem.presentation;
 
+import javax.validation.constraints.NotEmpty;
+
 public class LoginForm {
+
+    @NotEmpty(message = "Please enter your username")
+    private String username;
+
+    @NotEmpty(message = "Please enter your password")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
