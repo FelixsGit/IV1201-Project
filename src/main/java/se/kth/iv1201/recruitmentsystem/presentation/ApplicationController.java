fa2cd1182e68eef447b6cf8e0792824706632867
FelixsGit <28272254+FelixsGit@users.noreply.github.com>
2019-01-31 13:42:11 +0100

    Add controller post & get mapping for Login

--- src/main/java/se/kth/iv1201/recruitmentsystem/presentation/ApplicationController.java
+++ src/main/java/se/kth/iv1201/recruitmentsystem/presentation/ApplicationController.java
@@ -17,11 +17,12 @@ public class ApplicationController {
 
     static final String DEFAULT_PAGE_URL = "/";
     static final String REGISTER_PAGE_URL = "register";
+    static final String LOGIN_PAGE_URL = "login";
     private static String REGISTER_FORM_OBJ_NAME = "registrationForm";
+    private static String LOGIN_FORM_OBJ_NAME = "loginForm";
 
     /**
      * No page is specified, redirect to the welcome page.
-     *
      * @return A response that redirects the browser to the welcome page.
      */
     @GetMapping(DEFAULT_PAGE_URL)
@@ -43,6 +44,20 @@ public class ApplicationController {
     }
 
     /**
+     * A get request for the login page.
+     * @param model Model objects used in the login page.
+     * @return The login page url.
+     */
+    @GetMapping(LOGIN_PAGE_URL)
+    public String showLoginView(Model model){
+        if(model.containsAttribute(LOGIN_FORM_OBJ_NAME)){
+            model.addAttribute(new LoginForm());
+        }
+        return LOGIN_PAGE_URL;
+    }
+
+
+    /**
      * The registration form has been submitted.
      * @param registrationForm Content of the registration form.
      * @param bindingResult Validation result for the registration form.
@@ -57,6 +72,25 @@ public class ApplicationController {
         /**Stuff*/
         model.addAttribute(new RegistrationForm());
         return REGISTER_PAGE_URL;
+    }
+
+    /**
+     * The login form has been submitted.
+     * @param loginForm  Content of the login form.
+     * @param bindingResult Validation result for the login form.
+     * @param model Model objects used by the login page.
+     * @return
+     */
+    @PostMapping(DEFAULT_PAGE_URL + LOGIN_PAGE_URL)
+    public String loginUser(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return LOGIN_PAGE_URL;
+        }
+        System.out.println(loginForm.getUsername());
+        System.out.println(loginForm.getUsername());
+        /**Stuff*/
+        model.addAttribute(new LoginForm());
+        return LOGIN_PAGE_URL;
     }
 
 }
