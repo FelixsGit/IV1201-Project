d0bdd17d7b2a32b40e05b1fa27c4bd4ebeca3916
Acander <adrianz@kth.se>
2019-01-31 15:01:04 +0100

    add implementation to form and application controller

--- src/main/java/se/kth/iv1201/recruitmentsystem/presentation/ApplicationForm.java
+++ src/main/java/se/kth/iv1201/recruitmentsystem/presentation/ApplicationForm.java
@@ -1,4 +1,50 @@
 package se.kth.iv1201.recruitmentsystem.presentation;
 
+import javax.validation.constraints.NotEmpty;
+
 public class ApplicationForm {
+
+    @NotEmpty()
+    String expertise;
+
+    @NotEmpty()
+    String fromDate;
+
+    @NotEmpty()
+    String toDate;
+
+    @NotEmpty()
+    String yearsOfExperience;
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(String yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
 }
