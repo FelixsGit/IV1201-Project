a298598b78635c897603bc43daa3de4c9e4cc12a
FelixsGit <28272254+FelixsGit@users.noreply.github.com>
2019-02-20 00:29:44 +0100

    Remove unused syntax

--- src/main/java/se/kth/iv1201/recruitmentsystem/presentation/app/SearchApplicationForm.java
+++ src/main/java/se/kth/iv1201/recruitmentsystem/presentation/app/SearchApplicationForm.java
@@ -5,23 +5,5 @@ package se.kth.iv1201.recruitmentsystem.presentation.app;
  */
 public class SearchApplicationForm {
 
-    private String email;
-    private String ssn;
-
-    public String getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
