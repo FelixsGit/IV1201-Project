37010e84e27c95f0148ac162ada1d333a7159058
FelixsGit <28272254+FelixsGit@users.noreply.github.com>
2019-02-21 12:37:38 +0100

    Add competenceForm to allow users to see a list of competences

---
+++ src/main/java/se/kth/iv1201/recruitmentsystem/presentation/app/CompetenceForm.java
@@ -0,0 +1,18 @@
+package se.kth.iv1201.recruitmentsystem.presentation.app;
+
+import se.kth.iv1201.recruitmentsystem.domain.Competence;
+
+import java.util.List;
+
+public class CompetenceForm {
+
+    public List<Competence> getCompetences() {
+        return competences;
+    }
+
+    public void setCompetences(List<Competence> competences) {
+        this.competences = competences;
+    }
+
+    private List<Competence> competences;
+}
